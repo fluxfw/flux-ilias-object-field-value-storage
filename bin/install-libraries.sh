49ae2a5305cee9b578b35eeb905b28fe183e6e45
fluxfw <fluxfw@bluewin.ch>
2023-07-27 09:24:56 +0200

    Style sheet manager

--- bin/install-libraries.sh
+++ bin/install-libraries.sh
@@ -23,12 +23,12 @@ installLibrary flux-authentication-backend https://github.com/fluxfw/flux-authen
 
 installLibrary flux-config-api https://github.com/fluxfw/flux-config-api/archive/refs/tags/v2023-06-08-1.tar.gz
 
-installLibrary flux-form https://github.com/fluxfw/flux-form/archive/refs/tags/v2023-07-10-1.tar.gz
+installLibrary flux-form https://github.com/fluxfw/flux-form/archive/refs/tags/v2023-07-27-1.tar.gz
 
 installLibrary flux-http-api https://github.com/fluxfw/flux-http-api/archive/refs/tags/v2023-06-19-1.tar.gz
 
-installLibrary flux-pwa-generator https://github.com/fluxfw/flux-pwa-generator/archive/refs/tags/v2023-07-21-1.tar.gz
+installLibrary flux-pwa-generator https://github.com/fluxfw/flux-pwa-generator/archive/refs/tags/v2023-07-27-1.tar.gz
 
 installLibrary flux-shutdown-handler https://github.com/fluxfw/flux-shutdown-handler/archive/refs/tags/v2023-03-16-1.tar.gz
 
-installLibrary flux-value-format https://github.com/fluxfw/flux-value-format/archive/refs/tags/v2023-07-10-1.tar.gz
+installLibrary flux-value-format https://github.com/fluxfw/flux-value-format/archive/refs/tags/v2023-07-27-1.tar.gz
